f4217aa28b8128646fcc91631fa44f74648be469
Thomas Hart II <myrridin@gmail.com>
2015-11-15 19:00:34 -0500

    Bob exercise

---
+++ bob/bob.exs
@@ -0,0 +1,23 @@
+defmodule Teenager do
+  def hey(input) do
+    cond do
+      yelling?(input) -> "Whoa, chill out!"
+      question?(input) -> "Sure."
+      nothing?(input) -> "Fine. Be that way!"
+      true -> "Whatever."
+    end
+  end
+
+  defp yelling?(input) do
+    String.upcase(input) == input && Regex.run(~r/[^\W\d_]/, input)
+  end
+
+  defp question?(input) do
+    String.at(input, -1) == "?"
+  end
+
+  defp nothing?(input) do
+    String.strip(input) == ""
+  end
+end
+
