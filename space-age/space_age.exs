aee8b3ce6346e277a7c2820fde123ac69e795544
Thomas Hart II <myrridin@gmail.com>
2015-11-28 16:14:05 -0500

    Space age exercise

---
+++ space-age/space_age.exs
@@ -0,0 +1,23 @@
+defmodule SpaceAge do
+  @type planet :: :mercury | :venus | :earth | :mars | :jupiter
+                | :saturn | :neptune | :uranus
+
+  @earth_years 31557600
+  @years %{
+    mercury: @earth_years * 0.2408467,
+    venus:   @earth_years * 0.61519726,
+    earth:   @earth_years * 1.0,
+    mars:    @earth_years * 1.8808158,
+    jupiter: @earth_years * 11.862615,
+    saturn:  @earth_years * 29.447498,
+    uranus:  @earth_years * 84.016846,
+    neptune: @earth_years * 164.79132
+  }
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet'.
+  """
+  @spec age_on(planet, pos_integer) :: float
+  def age_on(planet, seconds), do: (seconds / @years[planet])
+end
