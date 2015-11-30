c6f4e997471d477ddc7a76e82416f44f4b93b5f9
Thomas Hart II <myrridin@gmail.com>
2015-11-29 20:53:02 -0500

    Scrabble exercise

---
+++ scrabble-score/scrabble.exs
@@ -0,0 +1,20 @@
+defmodule Scrabble do
+  @scores %{ "A" => 1, "B" => 3, "C" => 3, "D" => 2, "E" => 1, "F" => 4,
+             "G" => 2, "H" => 4, "I" => 1, "J" => 8, "K" => 5, "L" => 1,
+             "M" => 3, "N" => 1, "O" => 1, "P" => 3, "Q" => 10, "R" => 1,
+             "S" => 1, "T" => 1, "U" => 1, "V" => 4, "W" => 4, "X" => 8,
+             "Y" => 4, "Z" => 10 }
+
+  @doc """
+  Calculate the scrabble score for the word.
+  """
+  @spec score(String.t) :: non_neg_integer
+  def score(word) do
+    String.strip(word) |> String.upcase |> String.graphemes |> _score(0)
+  end
+
+  defp _score([], score), do: score
+  defp _score([head | tail], score), do: _score(tail, score + score_for(head))
+
+  defp score_for(letter), do: @scores[letter]
+end
